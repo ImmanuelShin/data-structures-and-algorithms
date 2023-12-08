1d4541000556c75b9175135dd687644956db1d6c
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-08 18:43:50 +0000

    Added challenges 4-5

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -71,8 +71,7 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
-
+  return stores.reduce((acc, c) => acc.map((a, i) => c[i] + a));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -86,7 +85,9 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
-  // Solution code here...
+  let array = [];
+  hours.forEach((a, i) => { array.push({ sales: data[i] + ' cookies', time: a }); });
+  return array;
 };
 
 /* ------------------------------------------------------------------------------------------------
